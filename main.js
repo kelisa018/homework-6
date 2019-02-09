47c316f2699f6f4571a35301de455960ce8a37bd
kelisa018 <kelisa018@gmail.com>
2019-02-09 23:06:59 +0300

    task2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Task1
 var arr = [7, 13, 3, 19, 27];
 function howManySeven(i){
   var str = i.toString();
@@ -18,3 +19,23 @@ function getSevenCol(arr){
   return sum;
 }
 console.log(getSevenCol(arr));
+
+// Task2
+var arr = ["p", "o", "r", "m", "i", "g", "n", "m", "a", "g", "r"];
+var n = 0;
+var o = 0;
+var res = "";
+for (var i = 0; i < arr.length; i++){
+  if(n === 0){
+   res = res + arr[o];
+  }
+  else res = res + arr[arr.length - 1 - o];
+  if(n === 0){
+    n = 1;
+  }
+  else {
+    n = 0;
+    o = o + 1;
+  }
+}
+console.log(res);
